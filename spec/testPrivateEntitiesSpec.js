18a242a3f6d8f1b2165c10767a5116ac8374ed5a
gabicrisan <crisan.gabriel9@gmail.com>
2017-03-20 16:42:13 +0200

    cleanup and gitignore

--- spec/testPrivateEntitiesSpec.js
+++ spec/testPrivateEntitiesSpec.js
@@ -1,10 +1,10 @@
-describe("privateEntity", function() {
-  var { PrivateEntity } = require("../lib/index.js")
+describe('privateEntity', function () {
+  var { PrivateEntity } = require('../lib/index.js')
 
-  it("should pass", function(){
-    let testPrivateObj = {text1: 'abcdef', array1:[true, false, false]}
+  it('should pass', function () {
+    let testPrivateObj = {text1: 'abcdef', array1: [true, false, false]}
     let privateObj = new PrivateEntity('test', testPrivateObj)
-    let publicObj = privateObj.makePublic();
+    let publicObj = privateObj.makePublic()
     expect(publicObj).toEqual({ Text1: 'ABCDEF', Array1: [ true, false, false ] })
   })
-});
+})
--- spec/testPublicEntitiesSpec.js
+++ spec/testPublicEntitiesSpec.js
@@ -1,39 +1,38 @@
-describe("publicEntity", function() {
-  var { PublicEntity, CustomError } = require("../lib/index.js")
+describe('publicEntity', function () {
+  var { PublicEntity, CustomError } = require('../lib/index.js')
 
-  it("should pass", function(){
+  it('should pass', function () {
     let testPublicObj = { Text1: 'abcdef', Array1: [ true, false, false ] }
     let publicObj = new PublicEntity('test', testPublicObj)
-    let privateObj = publicObj.makePrivate();
+    let privateObj = publicObj.makePrivate()
     expect(privateObj).toEqual({ text1: 'abcdef', array1: [ true, false, false ] })
   })
 
-  it("should fail MANDATORY test", function(){
+  it('should fail MANDATORY test', function () {
     let testPublicObj = { Array1: [ true, false, false ] }
     let publicObj = new PublicEntity('test', testPublicObj)
-    let errorString = "MANDATORY check failed for test.Text1\ntest.Text1 is mandatory but was not found."
-    expect( function(){ publicObj.makePrivate(); }).toThrow(new CustomError(errorString, "error"));
+    let errorString = 'MANDATORY check failed for test.Text1\ntest.Text1 is mandatory but was not found.'
+    expect(function () { publicObj.makePrivate() }).toThrow(new CustomError(errorString, 'error'))
   })
 
-  it("should fail TYPE test", function(){
+  it('should fail TYPE test', function () {
     let testPublicObj = { Text1: 1, Array1: [ true, false, false ] }
     let publicObj = new PublicEntity('test', testPublicObj)
-    let errorString = "TYPE check failed for test.Text1\n typeof test.Text1 == number (string expected)"
-    expect( function(){ publicObj.makePrivate(); }).toThrow(new CustomError(errorString, "error"));
+    let errorString = 'TYPE check failed for test.Text1\n typeof test.Text1 == number (string expected)'
+    expect(function () { publicObj.makePrivate() }).toThrow(new CustomError(errorString, 'error'))
   })
 
-  it("should fail REGEX validation test", function(){
-    let testPublicObj = { Text1: "abcde", Array1: [ true, false, false ] }
+  it('should fail REGEX validation test', function () {
+    let testPublicObj = { Text1: 'abcde', Array1: [ true, false, false ] }
     let publicObj = new PublicEntity('test', testPublicObj)
-    let errorString = "VALIDATE check failed for test.Text1\n'abcde' doesn't match regex: /.{6,}$/";
-    expect( function(){ publicObj.makePrivate(); }).toThrow(new CustomError(errorString, "error"));
+    let errorString = 'VALIDATE check failed for test.Text1\n"abcde" doesn\'t match regex: /.{6,}$/'
+    expect(function () { publicObj.makePrivate() }).toThrow(new CustomError(errorString, 'error'))
   })
 
-  it("should fail FUNCTION validation test", function(){
-    let testPublicObj = { Text1: "abcdef", Array1: [ true, false ] }
+  it('should fail FUNCTION validation test', function () {
+    let testPublicObj = { Text1: 'abcdef', Array1: [ true, false ] }
     let publicObj = new PublicEntity('test', testPublicObj)
-    let errorString = "VALIDATE check failed for test.Array1\n'true,false' doesn't pass Fn: testArray";
-    expect( function(){ publicObj.makePrivate(); }).toThrow(new CustomError(errorString, "error"));
+    let errorString = 'VALIDATE check failed for test.Array1\n"true,false" doesn\'t pass Fn: testArray'
+    expect(function () { publicObj.makePrivate() }).toThrow(new CustomError(errorString, 'error'))
   })
-
-});
+})
